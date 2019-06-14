4285733a4699eb90f68461024be4290973c0f45f
bgklug <bgklug@alaska.edu>
2019-06-14 15:23:38 -0800

    Update paths to current machine

--- control_programs/swept_freq/run_sweep.sh
+++ control_programs/swept_freq/run_sweep.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd /home/odroid
-source .bashrc
-cd /home/odroid/Desktop/4UAF_USRP-master/control_programs/swept_freq
+#cd /home/odroid
+#source .bashrc
+#cd /home/odroid/Desktop/4UAF_USRP-master/control_programs/swept_freq
 ./swept_freq --start 2000 --stop 8000 --nsteps 30 --write
