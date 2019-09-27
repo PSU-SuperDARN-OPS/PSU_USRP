cb23e1290dbd3dd1bc874dddaf41de13411ad5f4
bgklug <bgklug@alaska.edu>
2019-09-27 13:08:57 -0800

    Add boost log

--- ionosonde_server/transceive.cpp
+++ ionosonde_server/transceive.cpp
@@ -5,6 +5,7 @@
 #include <uhd/utils/static.hpp>
 #include <uhd/usrp/multi_usrp.hpp>
 #include <uhd/exception.hpp>
+
 #include <boost/thread/thread.hpp>
 #include <boost/program_options.hpp>
 #include <boost/math/special_functions/round.hpp>
@@ -12,6 +13,8 @@
 #include <boost/format.hpp>
 #include <boost/algorithm/string/split.hpp>
 #include <boost/algorithm/string/classification.hpp>
+#include <boost/log/trivial.hpp>
+
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -24,44 +27,43 @@
 #include <fftw3.h>
 #include <cstdio>
 #include <cstdlib>
-
 #include <thread>
+
 #include "global_variables.h"
 
-typedef std::complex<int16_t>  sc16;
 extern int verbose;
 
 void tx_worker(
-    unsigned int bufflen,
-    uhd::tx_streamer::sptr tx_stream,
-    uhd::time_spec_t start_time,
-    std::complex<int16_t>* vec_ptr,
-    int end
+        unsigned int bufflen,
+        uhd::tx_streamer::sptr tx_stream,
+        uhd::time_spec_t start_time,
+        std::complex<int16_t> *vec_ptr,
+        int end
 );
 
 void rx_worker(
-    uhd::rx_streamer::sptr rx_stream,
-    unsigned int samps_per_pulse,
-    std::vector<std::complex<int16_t>* >& recv_ptr
+        uhd::rx_streamer::sptr rx_stream,
+        unsigned int samps_per_pulse,
+        std::vector<std::complex<int16_t> *> &recv_ptr
 );
 
 void transceive(
-    uhd::usrp::multi_usrp::sptr usrp,
-    uhd::tx_streamer::sptr tx_stream,
-    uhd::rx_streamer::sptr rx_stream,
-    unsigned int npulses,
-    float pulse_time,
-    //std::complex<int16_t>* txbuff,
-    std::vector<std::complex<int16_t> >* txbuff0,
-    std::vector<std::complex<int16_t> >* txbuff1,
-    float tx_ontime,
-    std::complex<int16_t>** outdata,
-    size_t samps_per_pulse){
+        uhd::usrp::multi_usrp::sptr usrp,
+        uhd::tx_streamer::sptr tx_stream,
+        uhd::rx_streamer::sptr rx_stream,
+        unsigned int npulses,
+        float pulse_time,
+        //std::complex<int16_t>* txbuff,
+        std::vector<std::complex<int16_t> > *txbuff0,
+        std::vector<std::complex<int16_t> > *txbuff1,
+        float tx_ontime,
+        std::complex<int16_t> **outdata,
+        size_t samps_per_pulse) {
 
     int debug = 0;
     int fverb = 1;
 
-    if (debug){
+    if (debug) {
         std::cout << "samps_per_pulse: " << samps_per_pulse << std::endl;
     }
 
@@ -81,58 +83,59 @@ void transceive(
     std::vector<std::complex<int16_t> *> vec_ptr;
     vec_ptr.resize(1);
     //vec_ptr[0] = &txbuff->front();
-    
-    usrp->set_gpio_attr("RXA","CTRL",0x0, TRTRIG_BIT); //GPIO mode
-    usrp->set_gpio_attr("RXA","DDR",TRTRIG_BIT, TRTRIG_BIT); //Direction out
-    
+
+    usrp->set_gpio_attr("RXA", "CTRL", 0x0, TRTRIG_BIT); //GPIO mode
+    usrp->set_gpio_attr("RXA", "DDR", TRTRIG_BIT, TRTRIG_BIT); //Direction out
+
     //create metadata tags for receive stream
     uhd::rx_metadata_t rxmd;
-    std::vector<std::complex<int16_t> > buff(samps_per_pulse,0);
+    std::vector<std::complex<int16_t> > buff(samps_per_pulse, 0);
     if (verbose) std::cout << "rx buff size: " << buff.size() << std::endl;
     if (verbose) std::cout << "tx buff size: " << txbuff0->size() << std::endl;
     uhd::stream_cmd_t stream_cmd = uhd::stream_cmd_t::STREAM_MODE_NUM_SAMPS_AND_DONE;
-    stream_cmd.num_samps = npulses*samps_per_pulse;
+    stream_cmd.num_samps = npulses * samps_per_pulse;
     stream_cmd.stream_now = false;
-    stream_cmd.time_spec = start_time + 22 / usrp->get_rx_rate(); //Digital hardware delay is 22 samples long.  Found by experiment.
-    
+    stream_cmd.time_spec =
+            start_time + 22 / usrp->get_rx_rate(); //Digital hardware delay is 22 samples long.  Found by experiment.
+
     //loop for every pulse in the sequence
     size_t spb;
-    std::vector<std::complex<int16_t>* > rx_dptr;
+    std::vector<std::complex<int16_t> *> rx_dptr;
     rx_dptr.resize(usrp->get_rx_num_channels());
     spb = tx_stream->get_max_num_samps();
     if (verbose) std::cout << "npulses: " << npulses << std::endl;
-    if (fverb) std::cout << "pulse_time: " <<pulse_time<<std::endl;
+    if (fverb) std::cout << "pulse_time: " << pulse_time << std::endl;
     boost::thread_group rx_threads;
     boost::thread_group tx_threads;
-    for (int ipulse=0; ipulse<npulses; ipulse++){
+    for (int ipulse = 0; ipulse < npulses; ipulse++) {
         if (debug) std::cout << "pulse number: " << ipulse << std::endl;
         std::cout << "A" << std::endl;
-        for (size_t ichan=0; ichan<usrp->get_rx_num_channels(); ichan++){
-         rx_dptr[ichan] = ipulse*samps_per_pulse + outdata[ichan];
+        for (size_t ichan = 0; ichan < usrp->get_rx_num_channels(); ichan++) {
+            rx_dptr[ichan] = ipulse * samps_per_pulse + outdata[ichan];
         }
-        
+
         float timeout = 4.1;
         std::cout << "B" << std::endl;
-        usrp->set_command_time(start_time-50e-6,0);
-        usrp->set_gpio_attr("RXA","OUT",T_BIT, 0x8100);
-        
-        if (ipulse==0){
+        usrp->set_command_time(start_time - 50e-6, 0);
+        usrp->set_gpio_attr("RXA", "OUT", T_BIT, 0x8100);
+
+        if (ipulse == 0) {
             if (fverb) std::cout << "time spec: " << stream_cmd.time_spec.get_real_secs() << std::endl;
             if (fverb) std::cout << "Issuing stream command to start collecting samples\n";
             usrp->issue_stream_cmd(stream_cmd);
             std::cout << "Command issued" << std::endl;
         }
         std::cout << "C" << std::endl;
-        usrp->set_command_time(start_time+tx_ontime,0);
-        usrp->set_gpio_attr("RXA","OUT",R_BIT, 0x8100);
+        usrp->set_command_time(start_time + tx_ontime, 0);
+        usrp->set_gpio_attr("RXA", "OUT", R_BIT, 0x8100);
 
 // sig gen mimmic
 // 2r=ct, r=100e3, c=3e8, --> t=667e-6
 // off one default rect pulse later (34us)
-  //      usrp->set_command_time(start_time+1667e-6,0);
-  //      usrp->set_gpio_attr("RXA","OUT",TRIG_BIT,TRIG_BIT);
-  //      usrp->set_command_time(start_time+1701e-6,0);
-  //      usrp->set_gpio_attr("RXA","OUT",0x0,TRIG_BIT); 
+        //      usrp->set_command_time(start_time+1667e-6,0);
+        //      usrp->set_gpio_attr("RXA","OUT",TRIG_BIT,TRIG_BIT);
+        //      usrp->set_command_time(start_time+1701e-6,0);
+        //      usrp->set_gpio_attr("RXA","OUT",0x0,TRIG_BIT);
 
 /*Below is for testing with signal analyzer and varying by complentary pulse*/
 /*
@@ -170,28 +173,28 @@ void transceive(
 */
 
         std::cout << "D" << std::endl;
-        size_t acc_samps=0;
-        if (ipulse%2 == 0) {
+        size_t acc_samps = 0;
+        if (ipulse % 2 == 0) {
             vec_ptr[0] = &txbuff0->front();
         }
 
-        if (ipulse%2 == 1) {
+        if (ipulse % 2 == 1) {
             vec_ptr[0] = &txbuff1->front();
         }
 
-        if (ipulse != npulses-1) {
-             tx_threads.create_thread(boost::bind(tx_worker,
-                 txbuff0->size(), tx_stream, start_time, vec_ptr[0], 0));
+        if (ipulse != npulses - 1) {
+            tx_threads.create_thread(boost::bind(tx_worker,
+                                                 txbuff0->size(), tx_stream, start_time, vec_ptr[0], 0));
         }
 
-        if (ipulse == npulses-1) {
-             tx_threads.create_thread(boost::bind(tx_worker,
-                 txbuff0->size(), tx_stream, start_time, vec_ptr[0], 1));
+        if (ipulse == npulses - 1) {
+            tx_threads.create_thread(boost::bind(tx_worker,
+                                                 txbuff0->size(), tx_stream, start_time, vec_ptr[0], 1));
         }
 
         rx_threads.join_all();
         rx_threads.create_thread(boost::bind(rx_worker,
-         rx_stream, samps_per_pulse, rx_dptr));
+                                             rx_stream, samps_per_pulse, rx_dptr));
 
         /* //This Loop Does Nothing, WHY?
         for (int k=0; k<samps_per_pulse; k++){
@@ -222,12 +225,12 @@ void transceive(
  * but allow other actions to occur concurrently.
  **********************************************************************/
 void tx_worker(
-    unsigned int bufflen,
-    uhd::tx_streamer::sptr tx_stream,
-    uhd::time_spec_t start_time,
-    std::complex<int16_t>* vec_ptr,
-    int end
-){
+        unsigned int bufflen,
+        uhd::tx_streamer::sptr tx_stream,
+        uhd::time_spec_t start_time,
+        std::complex<int16_t> *vec_ptr,
+        int end
+) {
     unsigned int acc_samps = 0;
 
     uhd::tx_metadata_t md;
@@ -238,13 +241,13 @@ void tx_worker(
     size_t spb = tx_stream->get_max_num_samps();
     if (spb > bufflen) spb = bufflen;
 
-	while(acc_samps < bufflen-spb){
-            size_t nsamples = tx_stream->send(vec_ptr, spb, md);
-            vec_ptr += spb;
-            acc_samps += nsamples;
-            //std::cout << acc_samps <<std::endl;
-            md.start_of_burst = false;
-            md.has_time_spec = false;
+    while (acc_samps < bufflen - spb) {
+        size_t nsamples = tx_stream->send(vec_ptr, spb, md);
+        vec_ptr += spb;
+        acc_samps += nsamples;
+        //std::cout << acc_samps <<std::endl;
+        md.start_of_burst = false;
+        md.has_time_spec = false;
     }
     // Now on the last packet
     if (end) md.end_of_burst = true;
@@ -253,15 +256,15 @@ void tx_worker(
 }
 
 void rx_worker(
-    uhd::rx_streamer::sptr rx_stream,
-    unsigned int samps_per_pulse,
-    std::vector<std::complex<int16_t>* >& recv_ptr
-){
+        uhd::rx_streamer::sptr rx_stream,
+        unsigned int samps_per_pulse,
+        std::vector<std::complex<int16_t> *> &recv_ptr
+) {
     uhd::rx_metadata_t rxmd;
     float timeout = 4.1;
     rxmd.error_code = uhd::rx_metadata_t::ERROR_CODE_NONE;
     size_t nrx_samples = rx_stream->recv(recv_ptr, samps_per_pulse, rxmd, timeout);
-    if (rxmd.error_code != uhd::rx_metadata_t::ERROR_CODE_NONE){
+    if (rxmd.error_code != uhd::rx_metadata_t::ERROR_CODE_NONE) {
         std::cerr << "Error!\t";
         std::cerr << rxmd.error_code << std::endl;
     }
